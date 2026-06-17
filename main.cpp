f2b2621f2bda66f8210068166b8a3fa7cb8aeb54
Keisha Reed <Keisha.reed01@icloud.com>
2026-06-16 22:22:46 -0400

    Added hobby console application

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Variables
+    string favoriteBrand;
+    string favoriteBadge;
+    int badgesRestored;
+    int hoursPerWeek;
+
+    // Ask user questions
+    cout << "What is your favorite automotive brand? ";
+    cin >> favoriteBrand;
+
+    cout << "What is your favorite badge or emblem to restore? ";
+    cin >> favoriteBadge;
+
+    cout << "How many badges have you restored? ";
+    cin >> badgesRestored;
+
+    cout << "How many hours per week do you spend customizing? ";
+    cin >> hoursPerWeek;
+
+    // Simple calculation
+    int totalHours = badgesRestored * hoursPerWeek;
+
+    // Display results
+    cout << endl;
+    cout << "You enjoy restoring " << favoriteBadge
+         << " emblems from " << favoriteBrand << "." << endl;
+
+    cout << "You have restored "
+         << badgesRestored
+         << " badges and spend "
+         << hoursPerWeek
+         << " hours per week customizing vehicles." << endl;
+
+    cout << "Estimated total hobby hours: "
+         << totalHours << endl;
+
+    return 0;
+}
